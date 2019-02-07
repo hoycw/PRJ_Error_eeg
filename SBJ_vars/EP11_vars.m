fe5d0abf2297db1d76c21b915496b88720e54dd9
sheila-steiner <scsteiner@berkeley.edu>
2019-02-07 11:12:55 -0800

    Updated EP_11vars

--- SBJ_vars/EP11_vars.m
+++ SBJ_vars/EP11_vars.m
@@ -12,7 +12,7 @@ ft_defaults
 %--------------------------------------
 SBJ_vars.SBJ = 'EP11';
 SBJ_vars.raw_file = 'pilot_11.bdf';
-SBJ_vars.bhv_file = 'maybeEP10_Pilot11_response_log_20181127141339.txt';
+SBJ_vars.bhv_file = 'Pilot11_2_response_log_20181128144407_rm1st3trl.txt';
 SBJ_vars.block_prefix = '';
 
 SBJ_vars.dirs.SBJ     = [root_dir 'PRJ_Error_eeg/data/' SBJ_vars.SBJ '/'];
@@ -47,17 +47,16 @@ SBJ_vars.ch_lab.replace = {}; % {{'final','EXG#'},{'final2','EXG#2'}}
 SBJ_vars.ch_lab.prefix  = '1-';    % before every channel
 SBJ_vars.ch_lab.suffix  = '';    % after every channel
 SBJ_vars.ch_lab.trigger = 'Status';
-SBJ_vars.ch_lab.bad     = {...
-    };
-%SBJ_vars.ref_exclude = {}; %exclude from the CAR
+SBJ_vars.ch_lab.bad     = {'P4', 'PO8', 'O2','FT7', 'T7', 'C6'};
+%f7, fc5SBJ_vars.ref_exclude = {}; %exclude from the CAR
 
-SBJ_vars.trial_reject_ix = [];
+SBJ_vars.trial_reject_ix = [192, 265, 288, 312, 346, 356, 362:366, 377, 436, 447, 487, 589];
 
 %--------------------------------------
-% Noise Notes
+% Noise Not es
 %--------------------------------------
 % recording info sheet notes:
-    %'PO7','O1','Iz','Oz'... % noisy channels
+    %'PO7','O1','Iz,','Oz'... % noisy channels
 % PSD Notes:
     %'AF3','CP2','Iz','Oz','O1','O2','PO3','PO7','PO8','POz' - PSD looks noisy
     %'F1' - strange flat PSD
