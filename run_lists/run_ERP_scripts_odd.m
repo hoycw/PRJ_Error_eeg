8f8e6088075840ecebf996b2668d438ea44013c1
sheila-steiner <scsteiner@berkeley.edu>
2019-12-03 17:37:12 -0800

    added odd run script

--- run_lists/run_ERP_scripts_odd.m
+++ run_lists/run_ERP_scripts_odd.m
@@ -6,15 +6,14 @@ else root_dir='/Volumes/hoycw_clust/';app_dir='/Users/colinhoy/Code/Apps/';end
 %%
 addpath([root_dir 'PRJ_Error_eeg/scripts/']);
 addpath([root_dir 'PRJ_Error_eeg/scripts/utils/']);
-addpath([app_dir 'fieldtrip/']);
+%addpath([app_dir 'fieldtrip/']);
 ft_defaults
 
 %% General parameters
-SBJs = {'EP06','EP07','EP08','EP10','EP11','EP14','EP15','EP16','EP17','EP18','EP19',...
-           'EEG01','EEG02','EEG03','EEG04','EEG06','EEG07','EEG08','EEG09','EEG10','EEG12'};
+SBJs = {'EEG01','EEG02','EEG03','EEG04','EEG06','EEG07','EEG08','EEG09','EEG10','EEG12'};
 
 %% Run preprocessing
-proc_id_ica = proc_id;
+proc_id_ica = 'odd_full_ft';
 gen_figs    = 0;
 fig_vis     = 'off';
 reject_visual = 0;
@@ -31,16 +30,17 @@ plot_final_check = 0;
 %     fprintf('%s preprocessed at %.1f s (SBJ time = %.1f)\n',SBJs{s},SBJ_times(s),elapsed);
 % end
 
+
 %% View basic ERPs
-proc_id    = 'eeg_full_ft';
-an_id      = 'ERP_Z4_F2t1_dm2t0_fl05t20';
-stat_conds = {'DifFB'};%,'DifOutS'};
+proc_id    = 'odd_full_ft';
+an_id      = 'ERP_Fz_S2t13_dm2t0_fl05t20';
+stat_conds = {'Odd'};%,'DifOutS'};
 save_fig   = 1;
 fig_vis    = 'on';
 fig_ftype  = 'png';
 
 for st_ix = 1:numel(stat_conds)
-    plt_id     = 'ts_F2to1_evnts_sigLine';
+    plt_id     = 'ts_F2to13_evnts_sigLine';
     for s = 1:numel(SBJs)
         SBJ03a_ERP_save(SBJs{s},proc_id,an_id);
         SBJ03b_ERP_plot(SBJs{s},stat_conds{st_ix},proc_id,an_id,plt_id,save_fig,...
@@ -49,18 +49,20 @@ for st_ix = 1:numel(stat_conds)
     SBJ03c_ERP_plot_grp(SBJs,stat_conds{st_ix},proc_id,an_id,plt_id,save_fig,...
         'fig_vis',fig_vis,'fig_ftype',fig_ftype);
     
-    plt_id = 'ts_F2to1_but_evnts_sigPatch';
+    plt_id = 'ts_F2to13_but_evnts_sigPatch';
     SBJ03c_ERP_plot_grp_butterfly(SBJs,stat_conds{st_ix},proc_id,an_id,plt_id,save_fig,...
         'fig_vis',fig_vis,'fig_ftype',fig_ftype);
     
     close all;
 end
 
+
+%{
 %% View difference wave ERPs
-proc_id    = 'eeg_full_ft';
-an_id      = 'ERP_Cz_F2t1_dm2t0_fl05t20';
-conditions = 'DifOutS';
-plt_id     = 'ts_F2to1_evnts_sigLine';
+proc_id    = 'odd_full_ft';
+an_id      = 'ERP_Z4_S2t13_dm2t0_fl05t20';
+conditions = 'DifOddOS';
+plt_id     = 'ts_F2to13_evnts_sigLine';
 save_fig   = 1;
 fig_vis    = 'on';
 fig_ftype  = 'png';
@@ -71,12 +73,14 @@ end
 SBJ03c_ERP_plot_grp_diffwave(SBJs,conditions,proc_id,an_id,plt_id,save_fig,...
         'fig_vis',fig_vis,'fig_ftype',fig_ftype);
 
-plt_id = 'ts_F2to1_but_evnts_sigPatch';
+plt_id = 'ts_F2to13_but_evnts_sigPatch';
 SBJ03c_ERP_plot_grp_diffwave_butterfly(SBJs,conditions,proc_id,an_id,plt_id,save_fig,...
         'fig_vis',fig_vis,'fig_ftype',fig_ftype);
 
+conditions = 'DifOddOS';
+%}
 %% ERP Stats: Window Mean
-proc_id    = 'eeg_full_ft';
+proc_id    = 'odd_full_ft';
 save_fig   = 1;
 fig_vis    = 'on';
 fig_ftype  = 'png';
@@ -84,41 +88,72 @@ fig_ftype  = 'png';
 %     SBJ03a_ERP_save(SBJs{s},proc_id,an_id);
 % end
 
-an_id      = 'ERP_Fz_F2t1_dm2t0_fl05t20';
-stat_id    = 'DifOut_anv_mn2t3_jk';
+an_id      = 'ERP_Z4_S2t13_dm2t0_fl05t20';
+stat_id    = 'DifOddOS_anv_mn2t3_jk';
 SBJ04c_ERP_grp_stats_ANOVA(SBJs,proc_id,an_id,stat_id,save_fig,...
         'fig_vis',fig_vis,'fig_ftype',fig_ftype);
 
-an_id      = 'ERP_Fz_F2t1_dm2t0_fl05t20';
-stat_id    = 'DifFB_anv_mn2t3_jk';
+an_id      = 'ERP_Z4_S2t13_dm2t0_fl05t20';
+stat_id    = 'DifOddTS_anv_mn2t3_jk';
 SBJ04c_ERP_grp_stats_ANOVA(SBJs,proc_id,an_id,stat_id,save_fig,...
         'fig_vis',fig_vis,'fig_ftype',fig_ftype);
 
-an_id      = 'ERP_Pz_F2t1_dm2t0_fl05t20';
-stat_id    = 'DifOut_anv_mn3t4_jk';
+an_id      = 'ERP_Z4_S2t13_dm2t0_fl05t20';
+stat_id    = 'DifOddOT_anv_mn2t3_jk';
 SBJ04c_ERP_grp_stats_ANOVA(SBJs,proc_id,an_id,stat_id,save_fig,...
         'fig_vis',fig_vis,'fig_ftype',fig_ftype);
 
-an_id      = 'ERP_Pz_F2t1_dm2t0_fl05t20';
-stat_id    = 'DifFB_anv_mn3t4_jk';
+an_id      = 'ERP_Z4_S2t13_dm2t0_fl05t20';
+stat_id    = 'DifOddOS_anv_mn3t4_jk';
+SBJ04c_ERP_grp_stats_ANOVA(SBJs,proc_id,an_id,stat_id,save_fig,...
+        'fig_vis',fig_vis,'fig_ftype',fig_ftype);
+
+an_id      = 'ERP_Z4_S2t13_dm2t0_fl05t20';
+stat_id    = 'DifOddTS_anv_mn3t4_jk';
+SBJ04c_ERP_grp_stats_ANOVA(SBJs,proc_id,an_id,stat_id,save_fig,...
+        'fig_vis',fig_vis,'fig_ftype',fig_ftype);
+
+an_id      = 'ERP_Z4_S2t13_dm2t0_fl05t20';
+stat_id    = 'DifOddOT_anv_mn3t4_jk';
+SBJ04c_ERP_grp_stats_ANOVA(SBJs,proc_id,an_id,stat_id,save_fig,...
+        'fig_vis',fig_vis,'fig_ftype',fig_ftype);
+
+an_id      = 'ERP_Z4_S2t13_dm2t0_fl05t20';
+stat_id    = 'DifOddOS_anv_mn1t2_jk';
+SBJ04c_ERP_grp_stats_ANOVA(SBJs,proc_id,an_id,stat_id,save_fig,...
+        'fig_vis',fig_vis,'fig_ftype',fig_ftype);
+
+an_id      = 'ERP_Z4_S2t13_dm2t0_fl05t20';
+stat_id    = 'DifOddTS_anv_mn1t2_jk';
+SBJ04c_ERP_grp_stats_ANOVA(SBJs,proc_id,an_id,stat_id,save_fig,...
+        'fig_vis',fig_vis,'fig_ftype',fig_ftype);
+
+an_id      = 'ERP_Z4_S2t13_dm2t0_fl05t20';
+stat_id    = 'DifOddOT_anv_mn1t2_jk';
 SBJ04c_ERP_grp_stats_ANOVA(SBJs,proc_id,an_id,stat_id,save_fig,...
         'fig_vis',fig_vis,'fig_ftype',fig_ftype);
 
 %% ERP Stats: Peak-to-Peak
-proc_id    = 'eeg_full_ft';
+proc_id    = 'odd_full_ft';
 save_fig   = 1;
 fig_vis    = 'on';
 fig_ftype  = 'png';
 
-an_id   = 'ERP_Fz_F2t1_dm2t0_fl05t20';
-stat_id = 'DifOut_anv_p2pFRN';
+an_id   = 'ERP_Fz_S2t13_dm2t0_fl05t20';
+stat_id = 'DifOddOS_anv_p2pFRN';
 SBJ04c_ERP_grp_stats_ANOVA(SBJs,proc_id,an_id,stat_id,save_fig,...
         'fig_vis',fig_vis,'fig_ftype',fig_ftype);
 
-an_id   = 'ERP_Fz_F2t1_dm2t0_fl05t20';
-stat_id = 'DifFB_anv_p2pFRN';
+an_id   = 'ERP_Fz_S2t13_dm2t0_fl05t20';
+stat_id = 'DifOddTS_anv_p2pFRN';
 SBJ04c_ERP_grp_stats_ANOVA(SBJs,proc_id,an_id,stat_id,save_fig,...
         'fig_vis',fig_vis,'fig_ftype',fig_ftype);
+    
+an_id   = 'ERP_Fz_S2t13_dm2t0_fl05t20';
+stat_id = 'DifOddOT_anv_p2pFRN';
+SBJ04c_ERP_grp_stats_ANOVA(SBJs,proc_id,an_id,stat_id,save_fig,...
+        'fig_vis',fig_vis,'fig_ftype',fig_ftype);
+
 
 %% ODDBALL 
 % odd_plt_id = 'ts_S2to13_evnts_sigPatch';
@@ -142,16 +177,3 @@ SBJ04c_ERP_grp_stats_ANOVA(SBJs,proc_id,an_id,stat_id,save_fig,...
 % PLOT_DiffWave_Group(SBJs, plt_id, an_id, 'off', 1, '.png', 'DifWL');
 % PLOT_DiffWave_Group(SBJs, plt_id, an_id, 'off', 1, '.png', 'DifEH');
 
-%% Run Oddball ERPs
-odd_SBJs = {'EEG01','EEG02','EEG03','EEG04','EEG06','EEG08'};
-
-proc_id   = 'odd_full_ft';
-an_id     = 'ERP_Cz_S2t13_flt5t20_st6';
-plt_id    = 'ts_S2to13_evnts_sigPatch';
-fig_vis   = 'on';
-save_fig  = 1;
-fig_ftype = 'png';
-
-for s = 4:numel(SBJs)
-    oddball_stats_plot(SBJs{s}, proc_id, plt_id, an_id, fig_vis, save_fig, fig_ftype)
-end
