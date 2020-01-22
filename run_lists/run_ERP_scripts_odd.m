f80c352910dab9b410eb4dae60c45842830856a9
hoycw <hoycw@berkeley.edu>
2020-01-21 17:11:17 -0800

    an_id for oddball ERP S2t1; SBJ03c updated with event_times

--- run_lists/run_ERP_scripts_odd.m
+++ run_lists/run_ERP_scripts_odd.m
@@ -6,60 +6,74 @@ else root_dir='/Volumes/hoycw_clust/';app_dir='/Users/colinhoy/Code/Apps/';end
 %%
 addpath([root_dir 'PRJ_Error_eeg/scripts/']);
 addpath([root_dir 'PRJ_Error_eeg/scripts/utils/']);
-%addpath([app_dir 'fieldtrip/']);
+addpath([app_dir 'fieldtrip/']);
 ft_defaults
 
 %% General parameters
-SBJs = {'EEG10','EEG12'};
+SBJs = {'EEG01','EEG02','EEG03','EEG04','EEG05','EEG06','EEG07','EEG08','EEG09','EEG10','EEG12'};
 
 %% Run preprocessing
-proc_id = 'odd_full_ft';
-proc_id_ica = 'odd_full_ft';
-gen_figs    = 0;
-fig_vis     = 'off';
-reject_visual = 0;
-plot_final_check = 0;
-
-SBJ_times = zeros(size(SBJs));
-tic;
-for s = 1:numel(SBJs)
-    SBJ00_raw_view(SBJs{s}, 1, proc_id, 1)
-    ODD01_preproc(SBJs{s}, proc_id)
-    ODD02a_artifact_rejection(SBJs{s}, proc_id, gen_figs, fig_vis)
-    SBJ02b_ica_rejection(SBJs{s}, proc_id, proc_id_ica, reject_visual);
-    SBJ02c_trial_rejection(SBJs{s}, proc_id, plot_final_check)
-    SBJ_times(s) = toc;
-    if s==1; elapsed = SBJ_times(s); else; elapsed = SBJ_times(s)-SBJ_times(s-1); end
-    fprintf('%s preprocessed at %.1f s (SBJ time = %.1f)\n',SBJs{s},SBJ_times(s),elapsed);
-end
-
+% proc_id = 'odd_full_ft';
+% proc_id_ica = 'odd_full_ft';
+% gen_figs    = 0;
+% fig_vis     = 'off';
+% reject_visual = 0;
+% plot_final_check = 0;
+% 
+% SBJ_times = zeros(size(SBJs));
+% tic;
+% for s = 1:numel(SBJs)
+%     SBJ00_raw_view(SBJs{s}, 1, proc_id, 1)
+%     ODD01_preproc(SBJs{s}, proc_id)
+%     ODD02a_artifact_rejection(SBJs{s}, proc_id, gen_figs, fig_vis)
+%     SBJ02b_ica_rejection(SBJs{s}, proc_id, proc_id_ica, reject_visual);
+%     SBJ02c_trial_rejection(SBJs{s}, proc_id, plot_final_check)
+%     SBJ_times(s) = toc;
+%     if s==1; elapsed = SBJ_times(s); else; elapsed = SBJ_times(s)-SBJ_times(s-1); end
+%     fprintf('%s preprocessed at %.1f s (SBJ time = %.1f)\n',SBJs{s},SBJ_times(s),elapsed);
+% end
 
 %% View basic ERPs
 proc_id    = 'odd_full_ft';
-an_id      = 'ERP_Fz_S2t13_dm2t0_fl05t20';
-stat_conds = {'Odd'};%,'DifOutS'};
+an_ids     = {'ERP_Fz_S2t1_dm2t0_fl05t20','ERP_Pz_S2t1_dm2t0_fl05t20'};
+conditions = 'Odd';
 save_fig   = 1;
 fig_vis    = 'on';
 fig_ftype  = 'png';
 
-for st_ix = 1:numel(stat_conds)
-    plt_id     = 'ts_F2to13_evnts_sigLine';
-    for s = 1:numel(SBJs)
-        SBJ03a_ERP_save(SBJs{s},proc_id,an_id);
-        SBJ03b_ERP_plot(SBJs{s},stat_conds{st_ix},proc_id,an_id,plt_id,save_fig,...
-            'fig_vis',fig_vis,'fig_ftype',fig_ftype);
-    end
-    SBJ03c_ERP_plot_grp(SBJs,stat_conds{st_ix},proc_id,an_id,plt_id,save_fig,...
+for an_ix = 1:numel(an_ids)
+    %     for s = 1:numel(SBJs)
+    %         SBJ03a_ERP_save(SBJs{s},proc_id,an_ids{an_ix});
+    %         plt_id     = 'ts_S2t1_evnts_sigLine';
+    %         SBJ03b_ERP_plot(SBJs{s},conditions,proc_id,an_ids{an_ix},plt_id,save_fig,...
+    %             'fig_vis',fig_vis,'fig_ftype',fig_ftype);
+    %     end
+    
+    % Group ERP plot
+    plt_id     = 'ts_S2t1_evnts_sigLine';
+    SBJ03c_ERP_plot_grp(SBJs,conditions,proc_id,an_ids{an_ix},plt_id,save_fig,...
         'fig_vis',fig_vis,'fig_ftype',fig_ftype);
     
-    plt_id = 'ts_F2to13_but_evnts_sigPatch';
-    SBJ03c_ERP_plot_grp_butterfly(SBJs,stat_conds{st_ix},proc_id,an_id,plt_id,save_fig,...
-        'fig_vis',fig_vis,'fig_ftype',fig_ftype);
+    % plt_id = 'ts_F2to13_but_evnts_sigPatch';
+    % SBJ03c_ERP_plot_grp_butterfly(SBJs,conditions,proc_id,an_ids{an_ix},plt_id,save_fig,...
+    %     'fig_vis',fig_vis,'fig_ftype',fig_ftype);
     
-    close all;
+    %close all;
 end
 
+% Group Topo Plot: P3
+an_id  = 'ERP_all_S2t1_dm2t0_fl05t20';
+plt_id = 'topo_F3t45';
+SBJ03c_ERP_plot_grp_topo_cond(SBJs,conditions,proc_id,an_id,plt_id,save_fig,...
+    'fig_vis',fig_vis,'fig_ftype',fig_ftype);
 
+% Group Topo Plot: N2
+an_id  = 'ERP_all_S2t1_dm2t0_fl05t20';
+plt_id = 'topo_F18t25';
+SBJ03c_ERP_plot_grp_topo_cond(SBJs,conditions,proc_id,an_id,plt_id,save_fig,...
+    'fig_vis',fig_vis,'fig_ftype',fig_ftype);
+
+%% OLD SHEILA STUFF:
 %{
 %% View difference wave ERPs
 proc_id    = 'odd_full_ft';
