ccb3800f6b2ac9e7f8f8835e4a1949749025bf00
sheila-steiner <scsteiner@berkeley.edu>
2020-02-10 12:27:07 -0800

    added rm script to run_lists/OLD

---
+++ run_lists/OLD/rm_Feb10_2020_Sheila.sh
@@ -0,0 +1,26 @@
+cd ../../..
+cd data/
+rm EEG05/04_proc/plot/EEG05component063*
+rm EEG06/04_proc/plot/EEG06component061*
+rm EEG06/04_proc/plot/EEG06component060*
+rm EEG07/04_proc/plot/EEG07component060*
+rm EEG07/04_proc/plot/EEG07component061*
+rm EEG08/04_proc/plot/EEG08component060*
+rm EEG08/04_proc/plot/EEG08component061*
+rm EEG08/04_proc/plot/EEG08component062*
+rm EEG08/04_proc/plot/EEG08component063*
+rm EEG08/04_proc/plot/EEG08component064*
+rm EEG10/04_proc/plot/EEG10component061*
+rm EEG10/04_proc/plot/EEG10component062*
+rm EEG10/04_proc/plot/EEG10component063*
+rm EP06/04_proc/plot/EP06component053*
+rm EP07/04_proc/plot/EP07component061*
+rm EP08/04_proc/plot/EP08component061*
+rm EP14/04_proc/plot/EP14component061*
+rm EP15/04_proc/plot/EP15component061*
+rm EP15/04_proc/plot/EP15component062*
+rm EP16/04_proc/plot/EP16component061*
+rm EP17/04_proc/plot/EP17component063*
+rm EP18/04_proc/plot/EP18component062*
+rm EP18/04_proc/plot/EP18component063*
+
