2a00f8381ebebffb61fef644804d613975ea53ec
sheila-steiner <scsteiner@berkeley.edu>
2020-03-23 15:17:32 -0700

    fix null channels

---
+++ run_lists/run_fn_remove_null_channels.m
@@ -0,0 +1,7 @@
+function run_fn_remove_null_channels
+    SBJs = {'EEG17', 'EEG18', 'EEG19', 'EEG20', 'EEG21', 'EEG22', 'EEG23', 'EEG29'}
+    for index = 1:length(SBJs)
+        fn_remove_null_channels(SBJs{index}, 'eeg_full_ft')
+        fn_remove_null_channels(SBJs{index}, 'odd_full_ft')
+    end
+end
---
+++ utils/fn_remove_null_channels.m
@@ -0,0 +1,30 @@
+function fn_remove_null_channels(SBJ, proc_id)
+%% Check which root directory
+if exist('/home/knight/','dir');root_dir='/home/knight/';ft_dir=[root_dir 'PRJ_Error_eeg/Apps/fieldtrip/'];
+elseif exist('/Users/sheilasteiner/','dir'); root_dir='/Users/sheilasteiner/Desktop/Knight_Lab/';ft_dir='/Users/sheilasteiner/Downloads/fieldtrip-master/';
+elseif exist('Users/aasthashah/', 'dir'); root_dir = 'Users/aasthashah/Desktop/'; ft_dir = 'Users/aasthashah/Applications/fieldtrip';
+else root_dir='/Volumes/hoycw_clust/';ft_dir='/Users/colinhoy/Code/Apps/fieldtrip/';end
+
+addpath([root_dir 'PRJ_Error_eeg/scripts/']);
+addpath([root_dir 'PRJ_Error_eeg/scripts/utils/']);
+addpath(ft_dir);
+ft_defaults
+
+%% Set up processing and SBJ variables
+SBJ_vars_cmd = ['run ' root_dir 'PRJ_Error_eeg/scripts/SBJ_vars/' SBJ '_vars.m'];
+eval(SBJ_vars_cmd);
+
+load([SBJ_vars.dirs.preproc SBJ '_' proc_id '_final.mat']);
+
+null_neg = cell(size(SBJ_vars.ch_lab.null));
+for null_ix = 1:numel(SBJ_vars.ch_lab.null)
+    null_neg{null_ix} = ['-' SBJ_vars.ch_lab.prefix SBJ_vars.ch_lab.null{null_ix} SBJ_vars.ch_lab.suffix];
+end
+
+cfg = [];
+cfg.channel = [{'all'}, null_neg];
+clean_trials = ft_selectdata(cfg, clean_trials);
+
+clean_data_fname = [SBJ_vars.dirs.preproc SBJ '_' proc_id '_final.mat'];
+save(clean_data_fname, '-v7.3', 'clean_trials');
+end
