fa78d68822e09830587ce6d033850dc2563ba55c
hoycw <hoycw@berkeley.edu>
2020-04-08 11:59:14 -0700

    Happy Birthday to me! Here's a super awesome smart model prediciton figure that says why I was unwittingly awesome to myself

---
+++ utils/fn_min_white_space.m
@@ -0,0 +1,13 @@
+function [ax] = fn_min_white_space(orig)
+% Minimize the white space on a figure
+
+ax = orig;
+outerpos = ax.OuterPosition;
+ti = ax.TightInset; 
+left = outerpos(1) + ti(1);
+bottom = outerpos(2) + ti(2);
+ax_width = outerpos(3) - ti(1) - ti(3);
+ax_height = outerpos(4) - ti(2) - ti(4);
+ax.Position = [left bottom ax_width ax_height];
+
+end
