8153bcb8ae0983b670308dd50a9651150fc7fcc9
hoycw <hoycw@berkeley.edu>
2020-04-14 17:04:08 -0700

    slight change to SBJ_load

--- utils/fn_get_root_dir.m
+++ utils/fn_get_root_dir.m
@@ -6,7 +6,7 @@ if exist('/home/knight/','dir')
 elseif exist('/Volumes/hoycw_clust/','dir')
     root_dir = '/Volumes/hoycw_clust/';
     app_dir   = '/Users/colinhoy/Code/Apps/';
-elseif exist('/Users/sheilasteiner/','dir');
+elseif exist('/Users/sheilasteiner/','dir')
     root_dir='/Users/sheilasteiner/Desktop/Knight_Lab/';
     app_dir='/Users/sheilasteiner/Documents/MATLAB/';
 else
